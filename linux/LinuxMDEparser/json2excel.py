fa3275bd0368e3df824d2d2e6917365fa1f165b4
bmrodrigues <rodrigues.bruno@microsoft.com>
2021-04-09 19:05:17 +0100

    Perfomance changes, new instructions and version

--- linux/LinuxMDEparser/json2excel.py
+++ linux/LinuxMDEparser/json2excel.py
@@ -2,20 +2,20 @@ import json
 import csv
 
 class Json2excel:
-    def __init__(self, logfile, dictionaykey, filename):
+    def __init__(self, logfile, filename):
         self.logfile = logfile
-        self.dictionaykey = dictionaykey
         self.filename = filename
     
     def json2excel(self):
         try:
             with open (self.logfile) as json_file:
                 log_dict = json.load(json_file)
-                keys = log_dict[self.dictionaykey][0].keys()
+                dictkey = next(iter(log_dict))
+                keys = log_dict[dictkey][0].keys()
                 with open(self.filename, 'w', newline='', encoding = 'utf-8') as csvfile:
                     writer = csv.DictWriter(csvfile, fieldnames = keys)
                     writer.writeheader()
-                    writer.writerows(log_dict['scans'])
+                    writer.writerows(log_dict[dictkey])
                     print(f'{self.filename} created')
         except Exception as e:
             print(f'Someting went wrong:{e}')
