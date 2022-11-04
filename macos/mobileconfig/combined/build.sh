f3b5d1cb31b95dce3d3315b6468395c19a5e3e7b
Max Velitchko <maximvelichko@gmail.com>
2022-11-04 10:19:14 -0700

    Combined profiles updated

--- macos/mobileconfig/combined/build.sh
+++ macos/mobileconfig/combined/build.sh
@@ -4,7 +4,20 @@ ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $ROOTDIR
 
 echo "Building mdatp.mobileconfig..."
-./build_combined.py --template template.mobileconfig --out mdatp.mobileconfig --in ../profiles/kext.mobileconfig ../profiles/notif.mobileconfig ../profiles/fulldisk.mobileconfig ../profiles/sysext.mobileconfig ../profiles/netfilter.mobileconfig ../profiles/accessibility.mobileconfig
+./build_combined.py --template template.mobileconfig --out mdatp.mobileconfig --in \
+    ../profiles/kext.mobileconfig \
+    ../profiles/notif.mobileconfig \
+    ../profiles/fulldisk.mobileconfig \
+    ../profiles/sysext.mobileconfig \
+    ../profiles/netfilter.mobileconfig \
+    ../profiles/accessibility.mobileconfig \
+    ../profiles/background_services.mobileconfig
 
 echo "Building mdatp-nokext.mobileconfig..."
-./build_combined.py --template template.mobileconfig --out mdatp-nokext.mobileconfig --in ../profiles/notif.mobileconfig ../profiles/fulldisk.mobileconfig ../profiles/sysext.mobileconfig ../profiles/netfilter.mobileconfig ../profiles/accessibility.mobileconfig
+./build_combined.py --template template.mobileconfig --out mdatp-nokext.mobileconfig --in \
+    ../profiles/notif.mobileconfig \
+    ../profiles/fulldisk.mobileconfig \
+    ../profiles/sysext.mobileconfig \
+    ../profiles/netfilter.mobileconfig \
+    ../profiles/accessibility.mobileconfig \
+    ../profiles/background_services.mobileconfig
