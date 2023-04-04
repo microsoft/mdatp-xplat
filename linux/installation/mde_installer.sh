d8b256ff36b082bc7c23b5b39cf8e0fc23ab5b30
M. Smit <marijn@quovide.com>
2023-04-04 12:52:28 +0200

    Quick fix for wrong apt source on Ubuntu 22.04

--- linux/installation/mde_installer.sh
+++ linux/installation/mde_installer.sh
@@ -832,7 +832,7 @@ scale_version_id()
         else
             script_exit "unsupported version: $DISTRO $VERSION" $ERR_UNSUPPORTED_VERSION
         fi
-    elif [ $DISTRO == "ubuntu" ] && [[ $VERSION != "16.04" ]] && [[ $VERSION != "18.04" ]] && [[ $VERSION != "20.04" ]]; then
+    elif [ $DISTRO == "ubuntu" ] && [[ $VERSION != "16.04" ]] && [[ $VERSION != "18.04" ]] && [[ $VERSION != "20.04" ]] && [[ $VERSION != "22.04" ]]; then
         SCALED_VERSION=18.04
     else
         # no problems with 
