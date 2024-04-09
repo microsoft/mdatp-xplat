005da0fb6e00ef8b50a294fa0cf749f95ebdbcbd
Max Velitchko <13109133+maximvelichko@users.noreply.github.com>
2024-04-08 23:16:29 -0700

    macOS config profiles - remove kext from the combined one, assign proper identifiers

--- macos/mobileconfig/combined/build.sh
+++ macos/mobileconfig/combined/build.sh
@@ -5,19 +5,9 @@ cd $ROOTDIR
 
 echo "Building mdatp.mobileconfig..."
 ./build_combined.py --template template.mobileconfig --out mdatp.mobileconfig --in \
-    ../profiles/kext.mobileconfig \
-    ../profiles/notif.mobileconfig \
-    ../profiles/fulldisk.mobileconfig \
-    ../profiles/sysext.mobileconfig \
-    ../profiles/netfilter.mobileconfig \
     ../profiles/accessibility.mobileconfig \
-    ../profiles/background_services.mobileconfig
-
-echo "Building mdatp-nokext.mobileconfig..."
-./build_combined.py --template template.mobileconfig --out mdatp-nokext.mobileconfig --in \
-    ../profiles/notif.mobileconfig \
+    ../profiles/background_services.mobileconfig \
     ../profiles/fulldisk.mobileconfig \
-    ../profiles/sysext.mobileconfig \
     ../profiles/netfilter.mobileconfig \
-    ../profiles/accessibility.mobileconfig \
-    ../profiles/background_services.mobileconfig
+    ../profiles/notif.mobileconfig \
+    ../profiles/sysext.mobileconfig
